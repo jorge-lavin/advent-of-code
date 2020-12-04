b8d1c736260ca71c66e1ed344745f84fe23915bc
lavinj87 <7030463+lavinj87@users.noreply.github.com>
2020-12-04 21:42:22 +0100

    broken day_3

--- internal/days/2020/day_3/day_3.go
+++ internal/days/2020/day_3/day_3.go
@@ -1,6 +1,58 @@
 package days
 
+import (
+	"strings"
+)
+
 func day3() {}
+
+type world struct {
+	rows []string
+}
+
+func (m world) cursor(slope slope) *cursor {
+	return &cursor{world: m, slope: slope}
+}
+
+func (m world) pointAt(position position) byte {
+	if position.vertical >= uint(len(m.rows)) {
+		return 0
+	}
+
+	row := m.rows[position.vertical]
+
+	if position.horizontal >= uint(len(row)) {
+		return 0
+	}
+
+	return byte(row[position.horizontal])
+}
+
+func newWorld(description string) *world {
+	rows := strings.Split(description, "\n")
+	return &world{rows: rows}
+}
+
+type cursor struct {
+	world   world
+	slope   slope
+	current position
+}
+
+func (m *cursor) moveNext() {
+	m.current.horizontal += m.slope.horizontal
+	m.current.vertical += m.slope.vertical
+}
+
+type slope struct {
+	horizontal uint
+	vertical   uint
+}
+
+type position struct {
+	horizontal uint
+	vertical   uint
+}
 
 func isTree(point byte) bool {
 	return point == '#'
---
+++ internal/days/2020/day_3/day_3_test.go
@@ -0,0 +1,109 @@
+package days
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func Test_isTree(t *testing.T) {
+	tests := []struct {
+		point    byte
+		expected bool
+	}{{
+		point:    '#',
+		expected: true,
+	}, {
+		point:    '.',
+		expected: false,
+	}}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if actual := isTree(tt.point); actual != tt.expected {
+				t.Errorf("Expected:%v. Actual:%v", tt.expected, actual)
+			}
+		})
+	}
+}
+
+func Test_isOpen(t *testing.T) {
+	tests := []struct {
+		point    byte
+		expected bool
+	}{{
+		point:    '#',
+		expected: false,
+	}, {
+		point:    '.',
+		expected: true,
+	}}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if actual := isOpen(tt.point); actual != tt.expected {
+				t.Errorf("Expected:%v. Actual:%v", tt.expected, actual)
+			}
+		})
+	}
+}
+
+func Test_newWorld(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		worldDescription := "..##.......\n" + "#...#...#.."
+
+		actual := newWorld(worldDescription)
+		expected := &world{rows: []string{"..##.......", "#...#...#.."}}
+
+		if (!cmp.Equal(actual, expected, cmp.AllowUnexported(world{}))) {
+			t.Errorf("Expected:%v. Actual:%v", expected, actual)
+		}
+
+	})
+}
+
+func Test_moveNext(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		worldDescription := "..##.......\n" + "#...#...#.."
+
+		world := newWorld(worldDescription)
+		slope := slope{horizontal: 1, vertical: 2}
+		cursor := world.cursor(slope)
+		cursor.moveNext()
+
+		actual := cursor.current
+		expected := position{horizontal: 1, vertical: 2}
+
+		if !cmp.Equal(expected, actual, cmp.AllowUnexported(position{})) {
+			t.Errorf("Expected:%v, actual:%v", expected, actual)
+		}
+	})
+}
+
+func Test_pointAt(t *testing.T) {
+	worldDescription := "..##.......\n" + "#...#...#.."
+
+	world := newWorld(worldDescription)
+
+	tests := []struct {
+		position position
+		expected byte
+	}{{
+		position: position{0, 0},
+		expected: '.',
+	}, {
+		position: position{0, 2},
+		expected: '#',
+	}, {
+		position: position{2, 0},
+		expected: 'a',
+	}}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			if actual := world.pointAt(tt.position); actual != tt.expected {
+				t.Errorf("Expected:%c, actual:%c", tt.expected, actual)
+			}
+		})
+	}
+}
